965d4c03201bac7d84cf8777112536225f0975ca
benanddana92@gmail.com <benanddana92@gmail.com>
2017-03-09 14:25:53 -0800

    Created main menu.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
-
+	public static void main(String[] args) {
+		JFrame mainMenu = new JFrame("The Quadratic Solver");
+		JPanel panel = new JPanel();
+		mainMenu.add(panel);
+		JLabel label = new JLabel("Welcome to the Quadratic Solver, by Carter Weiss.");
+		JButton startButton = new JButton("Start \u2192");
+		panel.add(label);
+		panel.add(startButton);
+		mainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainMenu.setVisible(true);
+		mainMenu.pack();
+	}
 }
