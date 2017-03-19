933bcab9c5f4f3f8c624447898ef555314747957
benanddana92@gmail.com <benanddana92@gmail.com>
2017-03-19 13:21:28 -0700

    Adding if statements.

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,6 +1,19 @@
 
 public class Quadratic {
-	protected Quadratic(int a, int b, int c) {
-		
+	private int a; private int b; private int c;
+	
+	protected Quadratic(int aValue, int bValue, int cValue) {
+		a = aValue;
+		b = bValue;
+		c = cValue;
+	}
+	
+	// 2 values are always returned unless discriminant is 0
+	protected int[] solve() {
+		int[] returnStatement = new int[2];
+		if(Math.sqrt(a*a - 4*a*c) % 1 == 0) {
+			
+		}
+		return returnStatement;
 	}
 }
