24de2aa10ae6e27411b30d2ccb3be231f74a7ad3
benanddana92@gmail.com <benanddana92@gmail.com>
2017-06-11 08:24:14 -0700

    Fixed formatting and added more solution features

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,18 +1,21 @@
 
 public class Quadratic {
-	private int a; private int b; private int c;
-	
+	private int a;
+	private int b;
+	private int c;
+
 	protected Quadratic(int aValue, int bValue, int cValue) {
 		a = aValue;
 		b = bValue;
 		c = cValue;
 	}
-	
+
 	// 2 values are always returned unless discriminant is 0
 	protected int[] solve() {
 		int[] returnStatement = new int[2];
-		if(Math.sqrt(a*a - 4*a*c) % 1 == 0) {
-			returnStatement[0] = (int) (b*(-1) - Math.sqrt(a*a - 4*a*c)) / 2 *a;
+		if (Math.sqrt(a * a - 4 * a * c) % 1 == 0) {
+			returnStatement[0] = (int) (b * (-1) - Math.sqrt(a * a - 4 * a * c)) / 2 * a;
+			returnStatement[1] = (int) (b * (-1) - Math.sqrt(a * a + 4 * a * c)) / 2 * a;
 		}
 		return returnStatement;
 	}
